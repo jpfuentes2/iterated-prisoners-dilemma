9bbe0541d480a8ae4c0260b137d09c1cc7078e11
Andre Dublin <81dublin@gmail.com>
2014-11-22 23:20:30 -0500

    getting swifter
    Signed-off-by: Andre Dublin <81dublin@gmail.com>

--- ipd.swift
+++ ipd.swift
@@ -53,37 +53,30 @@ class Prisoner : Strategy {
 	}
 }
 
-func runGame(prisoners: [Prisoner], times: Int) {
-	func swap(ps: [Prisoner], fn: (mover: Prisoner, opp: Prisoner) -> ()) {
-		fn(mover: ps[0], opp: ps[1])
-		fn(mover: ps[1], opp: ps[0])
-	}
-	
-	for i in 0..<times {
-		swap(prisoners, { (mover: Prisoner, opp: Prisoner) in
-			mover.decisions.append(mover.move(opp))
-		})
-	}
-	
-	for i in 0..<prisoners[0].decisions.count {
-		swap(prisoners, { (mover: Prisoner, opp: Prisoner) in
-			let score = Trial()[mover.decisions[i], opp.decisions[i]]
-			mover.scores.append(score)
-			mover.totalScore += score
-		})
+func runGame<T: Prisoner>(ps: [T], times: Int) -> () -> (String, String) {
+	return { results in
+		func swap(ps: [T], fn: (mover: T, opp: T) -> ()) {
+			fn(mover: ps[0], opp: ps[1])
+			fn(mover: ps[1], opp: ps[0])
+		}
+		
+		for i in 0..<times {
+			swap(ps) { (mover: T, opp: T) in
+				mover.decisions.append(mover.move(opp))
+			}
+			
+			swap(ps) { (mover: T, opp: T) in
+				let score = Trial()[mover.decisions[i], opp.decisions[i]]
+				mover.scores.append(score)
+				mover.totalScore += score
+			}
+		}
+		
+		return ("Prisoner A (AlwaysCooperate): \(prisoners[0].totalScore)", "Prisoner B (AlwaysDefect): \(prisoners[1].totalScore)")
 	}
 }
 
-func main() {
-	let prisoners: [Prisoner] = [
-		Prisoner("a", AlwaysCooperate()),
-		Prisoner("b", AlwaysDefect())
-	]
-	
-	runGame(prisoners, 10)
-	
-	println("Prisoner A (AlwaysCooperate): \(prisoners[0].totalScore)")
-	println("Prisoner B (AlwaysDefect): \(prisoners[1].totalScore)")
-}
-
-main()
+let prisoners: [Prisoner] = [Prisoner("a", AlwaysCooperate()), Prisoner("b", AlwaysDefect())]
+let (PrisonerA, PrisonerB) = runGame(prisoners, 10)()
+println(PrisonerA)
+println(PrisonerB)
